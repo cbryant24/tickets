bd854df43cc63d2c621ba82e5fec51f888c5ebe5
Chris Bryant <chrisbryant@Chriss-MacBook-Pro-M1.local>
2021-04-13 00:48:41 -0700

    updated startup message

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,15 +1,16 @@
-import { natsWrapper } from './nats-wrapper';
-import { OrderCreatedListener } from './events/listeners/order-created-listeners';
+import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listeners";
 
 const start = async () => {
+  console.log("Starting...");
   if (!process.env.NATS_CLIENT_ID) {
-    throw new Error('NATS_CLIENT_ID must be defined');
+    throw new Error("NATS_CLIENT_ID must be defined");
   }
   if (!process.env.NATS_URL) {
-    throw new Error('NATS_URL must be defined');
+    throw new Error("NATS_URL must be defined");
   }
   if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error('NATS_CLUSTER_ID must be defined');
+    throw new Error("NATS_CLUSTER_ID must be defined");
   }
 
   try {
@@ -18,17 +19,17 @@ const start = async () => {
       process.env.NATS_CLIENT_ID,
       process.env.NATS_URL
     );
-    natsWrapper.client.on('close', () => {
-      console.log('NATS connection closed!');
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
       process.exit();
     });
-    process.on('SIGINT', () => natsWrapper.client.close());
-    process.on('SIGTERM', () => natsWrapper.client.close());
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
 
     new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) {
     console.error(err);
-    throw new Error('ERROR TRYING TO CONNECT TO SOMETHING');
+    throw new Error("ERROR TRYING TO CONNECT TO SOMETHING");
   }
 };
 
